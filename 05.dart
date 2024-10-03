3a8275b08ce3a83bfeea07bf8002545022cdaa1d
Herik Colares Rezende <dev.herik@gmail.com>
2024-10-03 11:03:32 -0300

    quinta questao resolvida

---
+++ 05.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  /* 5) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes.
+  Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser.
+  Seu objetivo é descobrir qual interruptor controla qual lâmpada. Como você faria para descobrir,
+  usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?*/
+
+  /* Resposta) Eu acenderia o primeiro e o segundo interruptor, e, após uns 2 minutos, apagaria o segundo; depois iria para a primeira
+  porta. Caso a lâmpada esteja acessa, é óbvio que seu interruptor é o primeiro; caso esteja apagada mas quente ao tocar, seu interruptor
+  será o segundo; caso esteja apagada e fria, seu interruptor é o terceiro.
+  Por fim, repetiria o mesmo processo para as duas últimas lâmpadas*/
+}
