f2c1d1ac300185ffa17ed4a6bc50016d9a40c776
Herik Colares Rezende <dev.herik@gmail.com>
2024-10-03 05:39:39 -0300

    quarta questao resolvida

---
+++ 04.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  //4) Descubra a lógica e complete o próximo elemento:
+  // a) 1, 3, 5, 7, [9] +2=9
+  // b) 2, 4, 8, 16, 32, 64, [128] *2=128
+  // c) 0, 1, 4, 9, 16, 25, 36, [49] diff+2=49
+  // d) 4, 16, 36, 64, [100] 12-20-28-36=100
+  // e) 1, 1, 2, 3, 5, 8, [13] fibonacci=13
+  // f) 2,10, 12, 16, 17, 18, 19, [200] todos começam com D
+}
